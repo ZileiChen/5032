6522e672511ae2c79ae83a675756dfa64bb84868
ZileiChen <chen.zilei@outlook.com>
2025-07-28 14:04:45 +1000

    first commit

---
+++ week1studio/src/components/MyName.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h3>Zilei Chen</h3>
+      <slot></slot>
+    </div>
+  </div>
+</template>
