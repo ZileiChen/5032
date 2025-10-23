3505679625ce84206bd24a549789834796e9d5c1
ZileiChen <chen.zilei@outlook.com>
2025-10-23 21:20:19 +1100

    fixing deployment

--- src/router/index.js
+++ src/router/index.js
@@ -52,7 +52,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHistory("/"),
   routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/5032/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
-
-module.exports = {
-  publicPath: import.meta.env.NODE_ENV === 'production' ? '/zchen-library/' : '/',
-}
+}))
